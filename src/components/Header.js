9350cabd5d957369087769358ef50b8aeb6fb3cb
secrojas <sec.rojas@gmail.com>
2020-11-06 17:25:48 -0300

    upload project 1.0

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <header className="bg-alert">
+            <h1>Busca Recetas de Bebidas</h1>
+            <h2 style={{textAlign:'center',color:'#e83e8c'}}>srojasweb.dev</h2>
+        </header>
+     );
+}
+ 
+export default Header;
